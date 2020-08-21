5d675a26100c0a662671c3d004928a01ea042f2e
JUAN FERNANDO GALEANO RUIZ <juangaleano50222@correo.itm.edu.co>
2020-08-20 22:05:08 -0500

    Add Deparments

---
+++ OnSale.Common/Entities/City.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnSale.Common.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The maximum characters allowed is 50")]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        [NotMapped]
+        public int IdDepartment { get; set; }
+
+    }
+
+}
--- OnSale.Common/Entities/Country.cs
+++ OnSale.Common/Entities/Country.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace OnSale.Common.Entities
 {
@@ -12,6 +11,12 @@ namespace OnSale.Common.Entities
         [MaxLength(50, ErrorMessage = "The maximum characters allowed is 50")]
         [Required]
         public string Name { get; set; }
+
+        public ICollection<Department> Departments { get; set; }
+
+        [DisplayName("Departments Number")]
+        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
     }
 
 }
+
