4f6ad10c4c2b4255cab42d7344ddc28a6f92cabb
JUAN FERNANDO GALEANO RUIZ <juangaleano50222@correo.itm.edu.co>
2020-08-30 12:45:49 -0500

    API with Security

---
+++ OnSale.Common/Requests/EmailRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnSale.Common.Requests
+{
+    public class EmailRequest
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+
+}
